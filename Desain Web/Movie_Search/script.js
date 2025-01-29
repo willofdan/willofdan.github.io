1577c0a58a0481e05524123f5ae54423167cc239
Danu Suko Handiyanto <162093149+willofdan@users.noreply.github.com>
2025-01-29 22:03:56 +0700

    Update script.js

--- Desain Web/Movie_Search/script.js
+++ Desain Web/Movie_Search/script.js
@@ -3,23 +3,78 @@ const buttSearch = document.querySelector(".button-search");
 buttSearch.addEventListener("click", () => {
   document.querySelector(".container-movie").innerHTML = "";
   const searchKey = document.querySelector(".input-key");
-  // Ubah http menjadi https
-  fetch("https://www.omdbapi.com/?apikey=f0e7bcba&s=" + searchKey.value)
+  fetch("http://www.omdbapi.com/?apikey=f0e7bcba&s=" + searchKey.value)
     .then((response) => response.json())
     .then((response) => {
-      if (response.Search) {  // Tambahkan pengecekan
-        const movie = response.Search;
-        movie.forEach((mov) => {
-          generateMovie(mov);
-        });
-      } else {
-        document.querySelector(".container-movie").innerHTML = 
-          "<h2>Movie not found!</h2>";
-      }
-    })
-    .catch(error => {  // Tambahkan error handling
-      console.error('Error:', error);
-      document.querySelector(".container-movie").innerHTML = 
-        "<h2>Something went wrong! Please try again.</h2>";
+      const movie = response.Search;
+      movie.forEach((mov) => {
+        generateMovie(mov);
+      });
     });
 });
+
+// IN CONSTRUCTION
+// document.addEventListener("click", function (e) {
+//   if (e.target.classList.contains("button-description")) {
+//     const imdbid = e.target.dataset.imdbid;
+
+//     fetch("http://www.omdbapi.com/?apikey=f0e7bcba&i=" + imdbid)
+//       .then((response) => response.json())
+//       .then((response) => getDescriptionMov(response));
+//   }
+// });
+
+function generateMovie({ Poster, Year, Title, imdbID }) {
+  const containerMov = document.querySelector(".container-movie");
+  const cards = document.createElement("div");
+  cards.classList.add("cards");
+  cards.innerHTML = `<div class="cards">
+                      <img src="${Poster}" />
+                      <div class="desc">
+                      <div>
+                        <h4 class="judul">${Title}</h4>
+                        <h3 class="tahun">${Year}</h3>
+                      </div>
+                         <button class="button-description" data-imdbid="${imdbID}">More Info</button>
+                      </div>
+                    </div>`;
+  containerMov.appendChild(cards);
+}
+
+// function getDescriptionMov({ Poster, Title, Genre, Released, Director, Plot }) {
+//   const container = document.createElement("div");
+
+//   container.classList.add("container-detail");
+//   const descriptionTable = ` <div class="detail">
+//   <span class="detail-exit">X</span>
+//     <div class="detail-poster">
+//      <img src="${Poster}"/>
+//        <table>
+//                                 <tr>
+//                                   <th>Judul</th>
+//                                   <td>${Title}</td>
+//                                 </tr>
+//                                 <tr>
+//                                   <th>Tanggal</th>
+//                                   <td>${Released}</td>
+//                                 </tr>
+//                                 <tr>
+//                                   <th>Director</th>
+//                                   <td>${Director}</td>
+//                                 </tr>
+//                                 <tr>
+//                                   <th>Genre</th>
+//                                   <td>${Genre}</td>
+//                                 </tr>
+//                                 <tr>
+//                                   <th>Plot</th>
+//                                   <td>${Plot}</td>
+//                                 </tr>
+//                             </table>
+//       </div>
+
+//                             </div>
+//                             `;
+//   container.innerHTML = descriptionTable;
+//   document.body.appendChild(container);
+// }
